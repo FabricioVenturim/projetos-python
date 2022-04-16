683713ae6f528419e12ab6bd36241896d2475532
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-04-16 10:28:01 -0300

    Quase Finalização

--- projetos_intermediarios/ler_arquivos/editor_texto.py
+++ projetos_intermediarios/ler_arquivos/editor_texto.py
@@ -6,25 +6,31 @@ from datetime import datetime
 #=======================================================================================
 def aplica_comandos(comandos, conteudo, arquivo_registro): 
     lista_palavras = comandos.split()
-    contador = 0
-    for palavra in lista_palavras:  #FAZER DUAS FUNÇÇÕES PARA ENCONTRAR OS PARÂMETROS
-        # essas duas linhas servem para encontrar a próxima palavra na lista, isso para o parâmetro da função
-        if contador < len(lista_palavras) - 1:
-            palavra_parametro = lista_palavras[contador + 1]
-            # esses try serve para conferir se existe mais duas palavras a frente, isso é, o segundo  parâmetro da função substituir.
-            try:
-                lista_palavras[contador + 2]
-            except:
-                break
-            else:
-                palavra_parametro_2 = lista_palavras[contador + 2]
-            contador+=1            
-        verifica_agrupar(palavra, conteudo, arquivo_registro)
-        verifica_maior(palavra, conteudo, arquivo_registro)
-        verifica_buscar(palavra, palavra_parametro, conteudo, arquivo_registro)
-        verifica_contar(palavra, palavra_parametro, conteudo, arquivo_registro)
-        verifica_substituir(palavra, palavra_parametro, palavra_parametro_2, conteudo, arquivo_registro)
-        
+    contador = 1
+    for palavra in lista_palavras: 
+        if contador == len(lista_palavras):
+            verifica_agrupar(palavra, conteudo, arquivo_registro)
+            verifica_maior(palavra, conteudo, arquivo_registro)
+            verifica_preguica(palavra, conteudo, arquivo_registro)
+            
+        elif contador == len(lista_palavras) - 1:
+            palavra_parametro_1 = lista_palavras[contador]
+            verifica_agrupar(palavra, conteudo, arquivo_registro)
+            verifica_maior(palavra, conteudo, arquivo_registro)
+            verifica_preguica(palavra, conteudo, arquivo_registro)
+            verifica_buscar(palavra, palavra_parametro_1, conteudo, arquivo_registro)
+            verifica_contar(palavra, palavra_parametro_1, conteudo, arquivo_registro)
+                       
+        else:
+            palavra_parametro_1 = lista_palavras[contador]
+            palavra_parametro_2 = lista_palavras[contador + 1]
+            verifica_agrupar(palavra, conteudo, arquivo_registro)
+            verifica_maior(palavra, conteudo, arquivo_registro)
+            verifica_preguica(palavra, conteudo, arquivo_registro)
+            verifica_buscar(palavra, palavra_parametro_1, conteudo, arquivo_registro)
+            verifica_contar(palavra, palavra_parametro_1, conteudo, arquivo_registro)
+            verifica_substituir(palavra, palavra_parametro_1, palavra_parametro_2, conteudo, arquivo_registro)
+        contador+=1     
     print(f"Arquivo {arquivo_registro} criado e escrito com sucesso!")
 
 #=======================================================================================
@@ -51,11 +57,14 @@ def verifica_maior(texto_comando, texto_conteudo, nome_registro):
 def verifica_buscar(texto_comando,palavra_parametro, texto_conteudo, nome_registro):
     padrao = re.compile(r".*Buscar$")
     if re.fullmatch(padrao, texto_comando):
-        pass
+        texto = buscar(palavra_parametro, texto_conteudo)
+        horario_execucao = datetime.now()
+        horario_execucao_arrumado = horario_execucao.strftime("%d/%m/%Y %H:%M:%S")
+        registrar(nome_registro,texto,"Buscar",horario_execucao_arrumado)
 
 
 def verifica_contar(texto_comando, palavra_parametro, texto_conteudo, nome_registro):
-    padrao = re.compile(r".*Contar$")
+    padrao = re.compile(r".*Contar$") 
     if re.fullmatch(padrao, texto_comando):
         texto = contar(palavra_parametro, texto_conteudo)
         horario_execucao = datetime.now()
@@ -75,11 +84,25 @@ def verifica_substituir(texto_comando, palavra_parametro, palavra_parametro_2, t
 def verifica_preguica(texto_comando, texto_conteudo, nome_registro):
     padrao = re.compile(r".*Preguiça.*")
     if re.fullmatch(padrao, texto_comando):
-        pass
+        texto = preguica(texto_conteudo)
+        horario_execucao = datetime.now()
+        horario_execucao_arrumado = horario_execucao.strftime("%d/%m/%Y %H:%M:%S")
+        registrar(nome_registro,texto,"Preguiça",horario_execucao_arrumado)
 
 #=======================================================================================
 # Funções das operações solicitadas
 #=======================================================================================
+def buscar(palavra, conteudo):
+    documento = conteudo.splitlines()
+    lista_de_linhas=[]
+    for linha in documento:
+        if re.search(palavra, linha):
+            lista_de_linhas.append(linha)
+    linhas_com_busca=""
+    for item in lista_de_linhas:
+        linhas_com_busca=linhas_com_busca+item+"\n"  #Arrumar depois
+    return linhas_com_busca
+
 def agrupar(frase):
     lista_palavras = list(frase)
     while " " in lista_palavras:
@@ -91,15 +114,15 @@ def agrupar(frase):
         frase_sem_espacos += i
     return frase_sem_espacos 
 
-def maior(frase):
-    frase = frase.split()
+def maior(conteudo):
+    texto = conteudo.split()
     tam_palavras = list()
-    for palavra in frase:
+    for palavra in texto:
         tam_palavras.append(len(palavra))
 
     maior = max(tam_palavras)
     maiores_palavras = ""
-    for a, b in zip(frase, tam_palavras):
+    for a, b in zip(texto, tam_palavras):
         if b == maior:
             maiores_palavras=maiores_palavras+a+" "
     return maiores_palavras
@@ -112,17 +135,17 @@ def contar(palavra, conteudo):
 
 def substituir (conteudo, palavra_antiga, palavra_nova):
     return conteudo.replace(palavra_antiga, palavra_nova)
+
+def preguica(conteudo):
+    return re.sub(r"\S\S\S\S\S(\S)*", "", conteudo) 
+
 #=======================================================================================
 # Funções que possibilitam a criação e escrita de registros
 #=======================================================================================
-
-def registrar(nome_registro, texto, operação, horario):
+def registrar(nome_registro, texto_resultado, operação, horario):
     with open(nome_registro, "a", encoding="utf-8") as arquivo_registro:
-        arquivo_registro.write("=============================================" + "\n")
-        arquivo_registro.write(operação + "\n")
-        arquivo_registro.write(horario + "\n")
-        arquivo_registro.write("Resultado:" + "\n")
-        arquivo_registro.write(texto)
-        arquivo_registro.write("\n" + "=============================================" + "\n")
+        arquivo_registro.write(operação + horario + "\n")
+        arquivo_registro.write(texto_resultado)
+        arquivo_registro.write("\n")
 
 
--- projetos_intermediarios/ler_arquivos/teste.py
+++
@@ -1,9 +0,0 @@
-def Substituir (texto, texto_antigo, texto_novo):
-    documento_novo=texto.replace(texto_antigo, texto_novo)
-    print(documento_novo)
-
-texto ="""
-"oi!! oi meu nome soi pablo, tchau!
-
-"""
-Substituir (texto, 'oi', 'tchau')
